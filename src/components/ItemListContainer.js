1c08a7d4820c23621e78933923878b68c670a51e
pablo a <pabloauterio@gmail.com>
2022-10-23 20:42:40 -0300

    Proyecto React

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,11 @@
+
+
+const ItemListContainer = ({Valor1, children}) =>{
+
+return(
+    <h1>Hola, {Valor1} <p>{children}</p></h1>
+      
+        )
+}
+
+export default ItemListContainer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+
+
+import logo from '../assets/img/logohuma.png';
+import CartWidget from './CartWidget';
+
+
+const Navbar = () =>{    
+    return(
+        <nav>
+            <div className="navbar">
+                <img src={logo} alt="logo"/>
+            <li><button>Home</button></li>
+            <li><select>
+        <option value='Productos'>Productos</option>
+        <option value='Cerámica'>Cerámica</option>    
+        <option value='Vidrio'>Vidrios</option>
+            </select></li>
+            <li><button>Info</button></li>
+                <CartWidget />
+                
+            </div>
+        </nav>
+
+    );
+
+};
+export default Navbar;
