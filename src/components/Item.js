1a913daf39f2c047663e42ec4e017ba3b8ef42d9
pablo a <pabloauterio@gmail.com>
2022-12-23 09:34:21 -0300

    mejorando

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,39 @@
-import React from "react";
-import oferta from '../assets/img/offer2.svg'
-
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Item = ({ info }) => {
+
+  const [toggle, setToggle] = useState(0.5)
+
+  // function makeBlur1() {
+  //   if (toggle === 0.5) {
+  //     setToggle(1)
+  //   } else {
+  //     setToggle(0.5)
+  //   }
+  //   document.body.style.opacity= toggle;
+  // }
+
+  const [clicked, setClicked] = useState('')
+
+  const getBigImage = () => {
+    setClicked(!clicked);
+//  makeBlur1();
+
+  }
+
+
   return (
     <div className="cards slideInLeft">
       <h2 className="oferta"> {info.destacado}</h2>
-      <img className="offer" src={info.offer} alt=''/>
+      <img className="offer" src={info.offer} alt='' />
 
-      <img className="imagenes" src={info.img} alt="xx"></img>
-      
+      <button className="btnNone" onClick={getBigImage}>
+        <img src={info.img} alt="xx" className={clicked ? 'big ' : 'imagenes'}>
+
+        </img>
+      </button>
+
       <p className="precio">Precio $ {info.precio}</p>
       <NavLink to={`/detalle/${info.id}`}>
         {" "}
