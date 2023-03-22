3914261d749a5a572ddee78a71f7e946a52e2753
pablo a <pabloauterio@gmail.com>
2023-03-21 21:30:59 -0300

    probando MUI

--- src/components/Images.js
+++ src/components/Images.js
@@ -20,22 +20,18 @@ const Images = () => {
     return (
   
     <Box>
-      <ImageList
-        sx={{ width: '100%', height: 450 }}
-        variant="quilted"
-        cols={4}
-        rowHeight={121}
-      >
-        {itemData.map((item) => (
-          <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
-            <img
-              {...srcset(item.img, 121, item.rows, item.cols)}
-              alt={item.title}
-              loading="lazy"
-            />
-          </ImageListItem>
-        ))}
-      </ImageList>
+            <ImageList variant="masonry" cols={3} gap={8}>
+                {itemData.map((item) => (
+                    <ImageListItem key={item.img}>
+                        <img
+                            src={`${item.img}?w=248&fit=crop&auto=format`}
+                            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                            alt={item.title}
+                            loading="lazy"
+                        />
+                    </ImageListItem>
+                ))}
+            </ImageList>
     </Box>
   
   )
