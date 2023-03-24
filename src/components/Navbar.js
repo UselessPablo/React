58c65ff2b4f0483ee7bb7f93f85b080e8cf58dba
pablo a <pabloauterio@gmail.com>
2023-03-24 09:43:32 -0300

    probando MUI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,18 @@
-
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import ButtonBase from '@mui/material/ButtonBase';
 import { NavLink, useNavigate } from 'react-router-dom';
 import '../index.css'
 import Cart from './CartWidget';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import React, { useState } from 'react';
+
 
 export const ScrollToTop = () => {
     window.scrollTo(-110, -100);
@@ -14,34 +24,73 @@ const Navbar = () => {
         navigate(path);
     }
     ScrollToTop();
+
+    const [open, setOpen] = useState(false);
+
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
+
     return (
 
-        <Box>
-            <nav className='sticky'>
-                <div className='navbar'>
+        
+        <>
+            <div>
+                <AppBar position="static">  
+                </AppBar>
+            
+                <nav className='sticky'>
+                    <div className='navbar'>
+                        <div className='logo'>
+                            <Toolbar >
 
-                    <div className='logo'>
-                        <h1 className=''>HumaBrc Cerámica</h1>
-                        <div className='catw'>
-                            <button className='cartWidget' onClick={cartBtn}><Cart /> </button>
+                                <ButtonBase sx={{ display: 'block' }} onClick={toggleDrawer}>
+                                    <IconButton edge="start" color="inherit" aria-label="menu">
+                                        <MenuIcon />
+                                    </IconButton>
+
+                                </ButtonBase>
+                                <Drawer anchor="left" open={open} onClose={toggleDrawer} >
+                                 
+                                    <List sx={{ backgroundColor: 'info2.main' }}>
+                                        <ListItemButton sx={{ backgroundColor: 'fondo.main', borderRadius:3, mt:10, mr:6, ml:1 }}>
+                                            <ListItemText primary="Galería" />
+                                        </ListItemButton>
+                                        <ListItemButton sx={{ backgroundColor: 'success.main', borderRadius: 3,mt:2, ml:6, mr:1 }}>
+                                            <ListItemText primary="WorkShops y Taller" />
+                                        </ListItemButton>
+                                        <ListItemButton sx={{ backgroundColor: 'eliminar.main', borderRadius: 3, mt: 2, mb: 16, mr: 6, ml: 1 }}>
+                                            <ListItemText primary="Contactame" />
+                                        </ListItemButton>
+                                    </List>
+                                </Drawer>
+                            </Toolbar>
+                            <h1 className=''>HumaBrc Cerámica</h1>
+                            <div className='catw'>
+                                <button className='cartWidget' onClick={cartBtn}><Cart /> </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </nav>
-            <div className='breadC' >
-                <NavLink className='breadText' to='/'>Home </NavLink> |
-                <NavLink className='breadText' to='/category/mate'>Mates</NavLink> |
-                <NavLink className='breadText' to='/category/maceta'>Macetas</NavLink> |
-                <NavLink className='breadText' to='/category/taza'>Tazas</NavLink> |
-                <NavLink className='breadText' to='/category/destacado'>Oferta</NavLink> |
-                <NavLink className='breadText' to='/category/galeria'>Varios</NavLink> |
-                <NavLink className='breadText' to='/pages/QuienesSomos'>Contacto</NavLink> |
-                <NavLink className='breadText' to='/Cart'>Carrito</NavLink>
+                </nav>
+                <div />
+            <div/>
             </div>
+                <Box>
+                <div className='breadC' >
+                    <NavLink className='breadText' to='/'>Home </NavLink> |
+                    <NavLink className='breadText' to='/category/mate'>Mates</NavLink> |
+                    <NavLink className='breadText' to='/category/maceta'>Macetas</NavLink> |
+                    <NavLink className='breadText' to='/category/taza'>Tazas</NavLink> |
+                    <NavLink className='breadText' to='/category/destacado'>Oferta</NavLink> |
+                    <NavLink className='breadText' to='/category/galeria'>Varios</NavLink> |
+                    <NavLink className='breadText' to='/pages/QuienesSomos'>Contacto</NavLink> |
+                    <NavLink className='breadText' to='/Cart'>Carrito</NavLink>
+                </div>
 
-        </Box>
+            </Box>
 
-
+        
+        </>
     );
 };
 
