db0f09b5a444746734c90fbd050aec6d0bf5e00f
pablo a <pabloauterio@gmail.com>
2023-04-04 19:10:13 -0300

    implementando searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,31 @@
 
-
-import * as React from 'react';
+import { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
 import { Box } from '@mui/material';
 import Autocomplete from '@mui/material/Autocomplete';
 import { useNavigate } from 'react-router-dom';
 import { getFirestore, collection, getDocs, query, where } from 'firebase/firestore';
-
-
+import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
 
 function SearchBar() {
-    const [searchTerm, setSearchTerm] = React.useState('');
-    const [options, setOptions] = React.useState([]);
-    const [selectedItemId, setSelectedItemId] = React.useState(null);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [options, setOptions] = useState([]);
+    const [selectedItemId, setSelectedItemId] = useState(null);
     const navigate = useNavigate();
 
-    const handleSearch = (event) => {
-        event.preventDefault();
+    useEffect(() => {
         const querydb = getFirestore();
         const queryCollection = collection(querydb, 'productos');
         if (searchTerm) {
-            const queryFilter = query(queryCollection, where('nombre', '==', searchTerm))
-            getDocs(queryFilter)
-                .then(res => setOptions(res.docs.map(productos => ({ id: productos.id, ...productos.data() }))))
-        // } else {
-        //     setOptions([options]);
-        // }
-    }
-    }
+            const searchQuery = query(queryCollection, where('nombre', '>=', searchTerm), where('nombre', '<', searchTerm + '\uf8ff'));
+            getDocs(searchQuery)
+                .then(res => setOptions(res.docs.map(productos => ({ id: productos.id, ...productos.data() }))));
+        } else {
+            return;
+        }
+    }, [searchTerm])
+
     const handleChange = (event, newValue) => {
         if (newValue) {
             setSelectedItemId(newValue.id);
@@ -36,40 +34,41 @@ function SearchBar() {
             setSelectedItemId(null);
         }
     };
-console.log(options);
+
     return (
         <Box sx={{ width: 220 }}>
-            <Autocomplete sx={{ borderRadius: 8, color:'info.main', height: 30, mt:1}}
-            disablePortal={false}
-            options={options}
-            getOptionLabel={(option) => option.nombre}
-            renderOption={(props, option) => (
-                <li  {...props} key={option.id} onClick={() => {
-                    setSelectedItemId(option.id);
-                    navigate(`/detalle2/${option.id}`);
-                }}>
-                    {option.nombre}
-                    <img className='mini' src={option.img} alt='x'></img>
-                </li>
-            )}
-            renderInput={(params) => (
-                <TextField sx={{color:'info2'}}
-                    {...params}
-                    label="Buscar"
-                    value={searchTerm}
-                    onChange={(event) => setSearchTerm(event.target.value)}
-                    onKeyDown={(event) => {
-                        if (event.key === 'Enter') {
-                            handleSearch(event);
-                        }
-                    }}
-                />
-            )}
-            onChange={handleChange}
-        />
+            <Autocomplete
+                sx={{ borderRadius: 8, color: 'fondo.main', height: 30, mt: 1 }}
+
+                disablePortal={true}
+                options={options}
+                getOptionLabel={(option) => option.nombre}
+                renderOption={(props, option) => (
+                    <li {...props} key={option.id} onClick={() => {
+                        setSelectedItemId(option.id);
+                        navigate(`/detalle2/${option.id}`);
+                    }}>
+                        {option.nombre}
+                        <img className='mini' src={option.img} alt='x'></img>
+                    </li>
+                )}
+                renderInput={(params) => (
+
+                    <TextField sx={{ color: 'info2', borderRadius: 4 }}
+                        {...params}
+                        label="Buscar..."
+                        value={searchTerm}
+                        onChange={(event) => setSearchTerm(event.target.value)}
+                       
+                    />
+                )}
+                onChange={handleChange}
+            />
         </Box>
     );
 }
+
 export default SearchBar;
+
 
 
