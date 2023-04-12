f1e93b08f3ec7fc2c6adb6f1aac88d25c9def8f2
pablo a <pabloauterio@gmail.com>
2023-04-12 10:51:12 -0300

    searchbar lista

--- src/pages/WorkShops.js
+++ src/pages/WorkShops.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Alert, Box, Grid,Typography, Stack } from '@mui/material'
+import {  Box, Grid,Typography, Stack } from '@mui/material'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
@@ -8,32 +8,32 @@ import dayjs, { Dayjs } from 'dayjs';
 
 const WorkShops = () => {
  
- const info =()=>{
+  const info = (selectedDate) => {
+    if (selectedDate.format('YYYY-MM-DD') === '2022-04-17') {
+      alert('Este sábado Workshop, hace tu tutor y llevatelo terminado.\n Costo Total por 4 hs: $10000')
+    }
+  }
 
-  alert('hola')
- }
- 
- 
  
   return (
  
-   <Box sx={{display:'flex', justifyContent:'center'}}>
+    <Box sx={{mt:3, display:'flex', justifyContent:'center'}} >
       <Grid container spacing={2}>
         <Grid sx={{ backgroundColor: 'info.main', display: 'flex', justifyContent: 'center',mt:3, alignContent:'center' }} item xs={6}>
-          <Typography variant='h5'> Vitrofusión </Typography>
+          <Typography variant='h6'> Vitrofusión </Typography>
+        </Grid>
+        <Grid sx={{ backgroundColor: 'info2.main', display: 'flex', justifyContent: 'center', mt: 3, pb: 2 }} item xs={6}>
+          <Typography variant='h6'> Cerámica </Typography>
+         
+        </Grid>
+
+
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DateCalendar defaultValue={dayjs('2022-04-17')} onChange={info}  />
           </LocalizationProvider>
-         
+          
         
-      </Grid>
-        
-        <Grid sx={{ backgroundColor: 'info2.main', display: 'flex', justifyContent: 'center',mt:3,pb:2 }} item xs={6}>
-          <Typography variant='h5'> Workshops cerámica </Typography>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateCalendar />
-          </LocalizationProvider>
-     </Grid>
+    
       </Grid>
     </Box>
   )
